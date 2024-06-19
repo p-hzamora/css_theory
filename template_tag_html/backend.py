c368fc37b5109bcb3b1c8a90968bc5351c7679dc
phzamora <p.hzamora@icloud.com>
2024-06-19 16:39:46 +0200

    update: added 'AddressValidator'

--- template_tag_html/backend.py
+++ template_tag_html/backend.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 from extension.orm.orm.condition_types import ConditionType
-from template_tag_html.models import AddressModel
+from template_tag_html.models.address import AddressModel, AddressValidator
 from extension.orm import MySQLRepository
 
 load_dotenv()
@@ -17,10 +17,15 @@ db = MySQLRepository(USER, PASSWORD, "sakila").connect()
 address_model = AddressModel(db)
 
 regex = r"^(A|B|C)"
-responsive = address_model.where(
+addresses, cities = address_model.where(
     lambda a: (a.City.city, ConditionType.REGEXP, regex), regex=regex
-).select(lambda a: (a, a.City.Country))
+).select(lambda a: (a, a.City))
 
 
-for a in responsive:
-    a
+for address in addresses:
+    validator = AddressValidator()
+    validate = validator.validate(address)
+    if not validate.is_valid:
+        for err in validate.errors:
+            print(err.ErrorMessage)
+print("Listado correcto")
--- template_tag_html/models/address.py
+++ template_tag_html/models/address.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from extension.fluent_validation import AbstractValidator
 
 from extension.orm import (
     Column,
@@ -19,9 +20,9 @@ class Address(Table):
     address2: str
     district: str
     city_id: int
-    postal_code: datetime
+    postal_code: str
     phone: str
-    location: datetime
+    location: bytes | str
     last_update: datetime = Column[datetime](is_auto_generated=True)
 
     City = ForeignKey["Address", City](
@@ -32,3 +33,29 @@ class Address(Table):
 class AddressModel(ModelBase[Address]):
     def __init__(self, repository: IRepositoryBase):
         super().__init__(Address, repository=repository)
+
+
+class AddressValidator(AbstractValidator[Address]):
+    def __init__(self) -> None:
+        super().__init__()
+        self.rule_for(lambda a: a.address_id).not_null().must(
+            lambda a: isinstance(a, int)
+        )
+        self.rule_for(lambda a: a.address).not_null().must(lambda a: isinstance(a, str))
+        self.rule_for(lambda a: a.address2).not_null().must(
+            lambda a: isinstance(a, str)
+        )
+        self.rule_for(lambda a: a.district).not_null().must(
+            lambda a: isinstance(a, str)
+        )
+        self.rule_for(lambda a: a.city_id).not_null().must(lambda a: isinstance(a, int))
+        self.rule_for(lambda a: a.postal_code).not_null().must(
+            lambda a: isinstance(a, str)
+        )
+        self.rule_for(lambda a: a.phone).not_null().must(lambda a: isinstance(a, str))
+        self.rule_for(lambda a: a.location).not_null().must(
+            lambda a: isinstance(a, bytes | str)
+        )
+        self.rule_for(lambda a: a.last_update).not_null().must(
+            lambda a: isinstance(a, datetime)
+        )
