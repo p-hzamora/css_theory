4143a5eb88ed921b1187a0809bedeab017b6f93b
phzamora <p.hzamora@icloud.com>
2024-06-19 21:58:16 +0200

    update: expediente_stc

--- expedientes_stc/script.js
+++ expedientes_stc/script.js
@@ -1,43 +1,107 @@
 const d = document;
 
+function read_response_and_fill_exp_list(){
 
-function fill_table_template(expediente, banco, coordinador, estado){
-    const $temp = d.querySelector("template-body-exp-table").content;
+    const $exp_container = d.querySelector(".exp-container");
     const $fragment = d.createDocumentFragment();
 
-    $temp.querySelector(".expediente").settAt
-    $temp.querySelector(".banco").settAt
-    $temp.querySelector(".coordinador").settAt
-    $temp.querySelector(".estado").settAt
 
-    expediente
-    banco
-    coordinador
-    estado
+    function fill_exp_list({expediente, banco, coordinador, estado}){
+        const $temp = d.getElementById("template-li-exp").content;
+
+        $temp.querySelector(".expediente").textContent = expediente;
+        $temp.querySelector(".banco").textContent = banco;
+        $temp.querySelector(".coordinador").textContent = coordinador;
+        $temp.querySelector(".estado").textContent = estado;
+
+        const clone = d.importNode($temp,true);
+        $fragment.appendChild(clone);
+
+    }
+
+    const response_pendientes=[
+        {
+            expediente: "qwertyui",
+            banco: "otro BBVA",
+            coordinador: "Pablo",
+            estado: "En progreso"
+        },
+        {
+            expediente: "1o2iu3yt",
+            banco: "caixa",
+            coordinador: "Marina",
+            estado: "Aperturando"
+        },
+        {
+            expediente: "xcvbnkjhgfds",
+            banco: "BBVA",
+            coordinador: "Pablo",
+            estado: "Abierto"
+        },
+    ]
+    const response_archivados=[
+        {
+            expediente: "12340132401",
+            banco: "BBVA",
+            coordinador: "Pablo",
+            estado: "Archivado"
+        },
+        {
+            expediente: "12340132401",
+            banco: "caixa",
+            coordinador: "Marina",
+            estado: "Cerrado"
+        },
+        {
+            expediente: "12340132401",
+            banco: "BBVA",
+            coordinador: "Pablo",
+            estado: "Abierto"
+        },
+    ]
+    const response_cerrados=[
+        {
+            expediente: "12340132401",
+            banco: "BBVA",
+            coordinador: "Pablo",
+            estado: "Archivado"
+        },
+        {
+            expediente: "12340132401",
+            banco: "caixa",
+            coordinador: "Marina",
+            estado: "Cerrado"
+        },
+        {
+            expediente: "12340132401",
+            banco: "BBVA",
+            coordinador: "Pablo",
+            estado: "Abierto"
+        },
+    ]
+
+
+
+    response_pendientes.forEach(e=>{
+        fill_exp_list(e);
+    });
+
+    $exp_container.appendChild($fragment);
 
 }
 
 
-const response=[
-    {
-        expediente: "12340132401",
-        banco: "BBVA",
-        coordinador: "Pablo",
-        estado: "Archivado"
-    },
-    {
-        expediente: "12340132401",
-        banco: "caixa",
-        coordinador: "Marina",
-        estado: "Cerrado"
-    },
-    {
-        expediente: "12340132401",
-        banco: "BBVA",
-        coordinador: "Pablo",
-        estado: "Abierto"
-    },
-]
+d.addEventListener("DOMContentLoaded",()=>{
+    const $estados = d.querySelectorAll(".estados .estado")
+    $estados.forEach(e=>{
+        e.addEventListener("click",function(){
+            $estados.forEach(e=>{
+                e.classList.remove("active")
+            })
+            this.classList.add("active")
+        })
+    })
+})
 
 
-fill_table_template();
+read_response_and_fill_exp_list()
