97e88908513b20a9b16f6780e7656ebd0af64b6e
p-hzamora <p.hzamora@icloud.com>
2024-06-28 08:52:36 +0200

    update: added StaffValidator class

--- template_tag_html/app/models/staff.py
+++ template_tag_html/app/models/staff.py
@@ -7,6 +7,8 @@ from app.extensions.orm import (
     ForeignKey,
 )
 
+from app.extensions.fluent_validation import AbstractValidator, CascadeMode
+
 from datetime import datetime
 
 from .address import Address
@@ -19,7 +21,7 @@ class Staff(Table):
     staff_id: int = Column[int](is_primary_key=True, is_auto_increment=True)
     first_name: str
     last_name: str
-    address_id: str
+    address_id: int
     picture: bytes
     email: str
     store_id: int
@@ -28,14 +30,31 @@ class Staff(Table):
     password: str
     last_update: datetime
 
-    Address = ForeignKey[Self, Address](
-        __table_name__, Address, lambda s, a: s.address_id == a.address_id
-    )
-    Store = ForeignKey[Self, Store](
-        __table_name__, Store, lambda staff, store: staff.store_id == store.store_id
-    )
+    Address = ForeignKey[Self, Address](__table_name__, Address, lambda s, a: s.address_id == a.address_id)
+    Store = ForeignKey[Self, Store](__table_name__, Store, lambda staff, store: staff.store_id == store.store_id)
 
 
 class StaffModel(ModelBase[Staff]):
     def __init__(self, repository: IRepositoryBase):
         super().__init__(Staff, repository=repository)
+
+
+def return_msg(dtype: object) -> str:
+    return "El tipo de dato {PropertyName} no es de tipo" + f"'{str(dtype)}'"
+
+
+class StaffValidator(AbstractValidator[Staff]):
+    def __init__(self) -> None:
+        super().__init__()
+        self.ClassLevelCascadeMode = CascadeMode.Continue
+        self.rule_for(lambda s: s.staff_id).must(lambda value: isinstance(value, int)).with_message(return_msg(int)).not_null()
+        self.rule_for(lambda s: s.first_name).must(lambda value: isinstance(value, str)).with_message(return_msg(str)).not_null().max_length(45)
+        self.rule_for(lambda s: s.last_name).must(lambda value: isinstance(value, str)).with_message(return_msg(str)).not_null().max_length(45)
+        self.rule_for(lambda s: s.address_id).must(lambda value: isinstance(value, int)).with_message(return_msg(int)).not_null()
+        self.rule_for(lambda s: s.picture).must(lambda value: isinstance(value, bytes | None)).with_message(return_msg(bytes))
+        self.rule_for(lambda s: s.email).must(lambda value: isinstance(value, str)).with_message(return_msg(str)).max_length(50).matches(r"^\S+@\S+\.\S+$").with_message("El campo introducido en la casilla de correo, no es un email valido").min_length(10)
+        self.rule_for(lambda s: s.store_id).must(lambda value: isinstance(value, int)).with_message(return_msg(int)).not_null()
+        self.rule_for(lambda s: s.active).must(lambda value: isinstance(value, int)).with_message(return_msg(int)).not_null().greater_than_or_equal_to(0).less_than_or_equal_to(10)
+        self.rule_for(lambda s: s.username).must(lambda value: isinstance(value, str)).with_message(return_msg(str)).not_null().max_length(16)
+        self.rule_for(lambda s: s.password).must(lambda value: isinstance(value, str)).with_message(return_msg(str)).max_length(40)
+        self.rule_for(lambda s: s.last_update).must(lambda value: isinstance(value, datetime | str)).with_message(return_msg(datetime)).not_null()
